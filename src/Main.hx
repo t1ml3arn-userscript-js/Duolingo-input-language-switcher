d8c773675bd3d5f199688de6987a254161563207
Tim <T1mL3arn@users.noreply.github.com>
2018-01-17 20:56:56 +1000

    refactor

--- src/Main.hx
+++ src/Main.hx
@@ -1,24 +1,17 @@
 class Main {
     static function main() {
-        trace("Duolingo input switcher inited");
         new Main();
     }
 
     var console:js.html.Console;
-    var origincons:js.html.Console;
     var document:js.html.Document;
 
     function new()
     {
-        console = js.Browser.window.console; 
         document = js.Browser.document;
-
-        console.error('wtf?');
-        origincons = console; // this does not help to bypass site polyfill
         console = untyped {};
 
         untyped (Object.assign(console, js.Browser.window.console));
-        // console.log('console after copy');
 
         /*
             run-at states:
@@ -37,8 +30,6 @@ class Main {
 
     function onready(?e)
     {
-        console.log('console copy after ready');
-        origincons.log('origin console copy after ready');
-        trace('it is just a trace()');
+        console.log("Duolingo input switcher inited");
     }
 }
