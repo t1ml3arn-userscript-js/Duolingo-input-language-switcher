eab36db16c818699b01779004447c9618c3892eb
Tim <T1mL3arn@users.noreply.github.com>
2018-01-18 16:28:26 +1000

    update meta

--- src/script-meta.js
+++ src/script-meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name Duolingo input switcher
 // @namespace https://www.duolingo.com/IVrL9
-// @match https://www.duolingo.com/
+// @match https://www.duolingo.com/*
+// @match https://www.example.com/*
+// @match https://example.com/*
 // @version 0.1
 // @description Swithes the letters to match the current task language. Allows you to type correct letters regardless of the keyboard layout
-// @require
-// @run-at document-end
-// @noframes
+// @run-at document-start
 // @grant none
 // ==/UserScript==
