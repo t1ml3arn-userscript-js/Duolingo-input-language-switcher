e6237b69353d81a98cedf791e39956c61d8864cc
Tim <T1mL3arn@users.noreply.github.com>
2018-01-22 19:47:23 +1000

    update version

--- src/script-meta.js
+++ src/script-meta.js
@@ -6,7 +6,7 @@
 // @match https://www.duolingo.com/practice/*
 // @match https://www.example.com/*
 // @match https://example.com/*
-// @version 0.1
+// @version 1.0
 // @description Swithes the letters to match the current task language. Allows you to type correct letters regardless of the keyboard layout
 // @run-at document-start
 // @grant none
