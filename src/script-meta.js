d644e95926e2fab1d8800844071de1b4c87b2e38
Tim <9349164+T1mL3arn@users.noreply.github.com>
2019-08-15 00:38:46 +1000

    release v1.5

--- src/script-meta.js
+++ src/script-meta.js
@@ -5,7 +5,7 @@
 // @match https://www.duolingo.com/*
 // @match https://www.example.com/*
 // @match https://example.com/*
-// @version 1.4.2
+// @version 1.5
 // @description This script allows you to type letters appropriate for current task without changing keyboard's layout
 // @run-at document-start
 // @grant none
