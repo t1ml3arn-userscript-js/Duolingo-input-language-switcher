5fa0c3318e499b254e733310f339782345423883
Tim <9349164+T1mL3arn@users.noreply.github.com>
2019-09-11 16:37:41 +1000

    add icon and up version to 2

--- src/script-meta.js
+++ src/script-meta.js
@@ -3,10 +3,11 @@
 // @namespace https://www.duolingo.com/IVrL9
 // @author T1mL3arn
 // @match https://www.duolingo.com/*
-// @version 1.5.1
+// @version 2
 // @description This script allows you to type letters appropriate for current task without changing keyboard's layout
 // @run-at document-start
 // @grant none
+// @icon https://d35aaqx5ub95lt.cloudfront.net/images/duolingo-touch-icon.png
 // @license GPLv3
 // @homepageURL https://github.com/T1mL3arn/Duolingo-input-language-switcher
 // @supportURL https://greasyfork.org/en/scripts/37693-duolingo-input-language-switcher/feedback
