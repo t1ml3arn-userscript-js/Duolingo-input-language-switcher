178d1d9fe089df6a528024bba1dda26ec019b4db
Tim <tim@noreplay.com>
2022-12-21 16:24:26 +1000

    release v3.0.0

--- src/script-meta.js
+++ src/script-meta.js
@@ -3,12 +3,13 @@
 // @namespace https://www.duolingo.com/IVrL9
 // @author T1mL3arn
 // @match https://www.duolingo.com/*
-// @version 2.0.3
-// @description This script allows you to type letters appropriate for current task without changing keyboard's layout
+// @version 3.0.0
+// @description This script allows you to type letters appropriate for current challenge without changing keyboard layout. Similar to Punto Switcher.
+// @description:ru Скрипт дает возможность выполнять упражнения не отвлекаясь на переключение раскладки клавиатуры. Похоже на Punto Switcher.
 // @run-at document-start
 // @grant none
 // @icon https://www.androidpolice.com/wp-content/uploads/2014/03/nexusae0_Duolingo-Thumb.png
-// @license GPLv3
+// @license GPL-3.0-only
 // @homepageURL https://github.com/T1mL3arn/Duolingo-input-language-switcher
 // @supportURL https://greasyfork.org/en/scripts/37693-duolingo-input-language-switcher/feedback
 // ==/UserScript==
